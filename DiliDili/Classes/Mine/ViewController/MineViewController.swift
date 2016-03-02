6a4082850c5647da10521a99f48a6fb8c05cf185
yq616775291 <616775291@qq.com>
2016-03-02 15:22:29 +0800

    Change Project Struct

---
+++ DiliDili/Classes/Mine/ViewController/MineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MineViewController.swift
+//  DiliDili
+//
+//  Created by yq on 16/3/2.
+//  Copyright © 2016年 yq. All rights reserved.
+//
+
+import UIKit
+
+class MineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
